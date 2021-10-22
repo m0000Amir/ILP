38127168edeb2e19f1396c76f9db2893438156c7
m0000a <mukhtarov.amir.a@gmail.com>
2021-10-22 18:49:19 +0300

    refactored pathloss module

--- pathloss/get_sta_value.m
+++ pathloss/get_sta_value.m
@@ -1,21 +1,26 @@
+% function [R, r] = get_sta_value(Ptr_link, Gtr_link, Precv_link, ...
+%     Precv_coverage, Grecv_coverage, ...
+%     Precv_gateway, Grecv_gateway, frequency, ...
+%     Ptr_ud, Gtr_ud)
 function [R, r] = get_sta_value(Ptr_link, Gtr_link, Precv_link, ...
-    Precv_coverage, Grecv_coverage, ...
-    Precv_gateway, Grecv_gateway, frequency, ...
-    Ptr_ud, Gtr_ud)
+    L_link, L_coverage, Precv_coverage, Grecv_coverage, ...
+    Precv_gateway, Grecv_gateway, Lrecv_gateway, frequency, ...
+    Ptr_ud, Gtr_ud, Ltr_ud)
 % Get link distance station R(i,j)
 % and
 % Get coverage radius r(i)
 
 %% CONST
-SOM = 10; %System Operating Margins
+SOM = 20; %System Operating Margins
+SOM_coverage = 14;
 F = frequency; % frequency
 K = - 27.55;
 % для частоты, выраженной в ГГц, и расстояния в киломметрах, K = 92,45
 % для частоты, выраженной в МГц, и расстояния в киломметрах, K = 32,4
 % для частоты, выраженной в МГц, и расстояния в метрах, K =  -27,55
-Ltr = 1; % TX cable loss
-Lrecv = 1;% RX cable loss
-Lrecv_coverage = 0;
+% Ltr = 1; % TX cable loss
+% Lrecv = 1;% RX cable loss
+% Lrecv_coverage = 0;
 % Gtr_coverage = 0;
 % Grecv_coverage = 0;
 % Precv_coverage = -67;
@@ -32,7 +37,8 @@ Lrecv_coverage = 0;
 % % coverage
 % Ptr_coverage = [20; 19; 20; 19; 20; 20; 20];
 
-BS = table(Ptr_link, Gtr_link, Precv_link, Precv_coverage, Grecv_coverage);
+BS = table(Ptr_link, Gtr_link, Precv_link, L_link, ...
+    Precv_coverage, Grecv_coverage, L_coverage);
 % 
 r = zeros(1, height(BS));
 R_sta = zeros(height(BS), height(BS));
@@ -41,27 +47,27 @@ for i = 1 : height(BS)
         if i == j
             R_sta(i,j) = inf;
         else
-            R_sta(i,j) = get_distance(BS.Ptr_link(i), Ltr, ...
-                BS.Gtr_link(i), BS.Gtr_link(j), Lrecv, SOM, ...
+            R_sta(i,j) = get_distance(BS.Ptr_link(i), L_link(i), ...
+                BS.Gtr_link(i), BS.Gtr_link(j), BS.L_link(i), SOM, ...
                 BS.Precv_link(j), F, K);
         end          
     end
-%     r(i) = get_distance(BS.Ptr_coverage(i), Ltr, BS.Gtr_coverage(i), ...
-%         Grecv_coverage, Lrecv_coverage, SOM, ...
-%         Precv_coverage, F, K);
-    r(i) = get_distance(Ptr_ud, 0, Gtr_ud, ... 
-        BS.Grecv_coverage(i), Lrecv, SOM, ...
+
+    r(i) = get_distance(Ptr_ud, Ltr_ud, Gtr_ud, ... 
+        BS.Grecv_coverage(i), BS.L_coverage(i), SOM_coverage, ...
         BS.Precv_coverage(i), F, K);
 end
 R_sta2gateway = zeros(height(BS), 1);
 
 for k = 1 : height(BS)
-    R_sta2gateway(k, 1) = get_distance(BS.Ptr_link(k), Ltr, ...
-        BS.Gtr_link(k, 1), Grecv_gateway, Lrecv, SOM, ...
+    R_sta2gateway(k, 1) = get_distance(BS.Ptr_link(k), BS.L_link(k), ...
+        BS.Gtr_link(k, 1), Grecv_gateway, Lrecv_gateway, SOM, ...
         Precv_gateway, F, K);
 end
 R1 = [R_sta, R_sta2gateway];
 R2 = [R_sta2gateway', inf];
 R = [R1; R2];
+r
+R
 
 end
